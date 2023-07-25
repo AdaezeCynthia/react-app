c6e373c162bf20784ec197ce6139f1baae8fad41
unknown <nackycynthia@gmail.com>
2023-07-26 00:49:29 +0100

    first commit

---
+++ bank-modern-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-primary w-full overflow-hidden'>
+      <hi>Hello world</hi>
+
+
+    </div>
+  )
+}
+
+export default App  
